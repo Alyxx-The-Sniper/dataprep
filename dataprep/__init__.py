f9f9ac81ef7e121a5c08f58c7ff28d9d8a4ff738
alyxx <75878508+Alyxx-The-Sniper@users.noreply.github.com>
2025-05-27 14:05:49 +0800

    edit init

--- dataprep/__init__.py
+++ dataprep/__init__.py
@@ -1,8 +1,9 @@
-from .transformers import NullImputer, DTypeCorrector, OutlierImputer, DataPipeline
+from .transformers import NullImputer, DTypeCorrector, OutlierImputer, DataPipeline, plot_violin_and_box
 
 __all__ = [
     "NullImputer",
     "DTypeCorrector",
     "OutlierImputer",
     "DataPipeline",
+    'plot_violin_and_box',
 ]
